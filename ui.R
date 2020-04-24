68ea9d796c3f05fac327b8236a0f0b32126e4bb2
Alex Ellman <ellmanalex@yahoo.com>
2020-04-24 14:49:22 -0400

    first commit, toolbar started on ui

---
+++ ui.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(shinythemes)
+
+fluidPage(
+  theme = ('simplex'),
+  navbarPage(
+    title = 'Cryptocurrency Market',
+    id = 'nav',
+    position = 'fixed-top'
+    )
+  )
+
+
+
